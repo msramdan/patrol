da0f10d21b032a5a0a5f6a0cb1f34de621a346dc
ahmadsofuwan <69912778+ahmadsofuwan@users.noreply.github.com>
2023-08-20 14:08:04 +0700

    update stack

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,18 @@ Route::middleware(['auth', 'web'])->group(function () {
     Route::prefix('profile')->group(function () {
         Route::get('/', [App\Http\Controllers\ProfileController::class, 'index'])->name('profile');
         Route::post('/save', [App\Http\Controllers\ProfileController::class, 'store'])->name('profile.save');
+        Route::get('/detail/{id}', [App\Http\Controllers\ProfileController::class, 'detail'])->name('profile.detail');
     });
     Route::prefix('suggestion')->group(function () {
         Route::get('/', [App\Http\Controllers\SuggestionController::class, 'index'])->name('suggestion');
         Route::post('/save', [App\Http\Controllers\SuggestionController::class, 'store'])->name('suggestion.save');
+    });
+
+
+    Route::prefix('admin')->group(function () {
+
+        Route::prefix('report')->group(function () {
+            Route::get('/', [App\Http\Controllers\Admin\ReportController::class, 'index'])->name('admin.report');
+        });
     });
 });
