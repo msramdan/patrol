36f62ce5a47c86d7bc73d378445e7e793abcf29d
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-08-20 09:32:20 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,7 +91,7 @@
                         </span>
                         <span>Jadwal Patroli</span>
                     </a></li>
-                <li><a class="nav-link" href="profile.html">
+                <li><a class="nav-link" href="{{ route('users.index') }}">
                         <span class="dz-icon">
                             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"
                                 fill="#000000">
