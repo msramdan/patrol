319fe4ae87131c446bda8e43203ce0b90d00140d
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-08-21 15:10:01 +0700

    update

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -14,7 +14,7 @@
         </div>
     </div>
     <ul class="nav navbar-nav">
-        @if (dataUser()->_level_user == 1)
+        @if (dataUser()->level_user == 1)
             <li><a class="nav-link" href="{{ route('admin.report') }}">
                     <span class="dz-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"
